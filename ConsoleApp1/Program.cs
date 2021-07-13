63908890d90d15d66bbd68846d8261362f1d32bb
gauravsarwal <gauravsarwal1487@gmail.com>
2021-07-13 12:24:08 +0530

    added feature 2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,8 +8,9 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             // code was edited in Github
-             // code was added in VS
-             //code to call feature 1
+            // code was added in VS
+            //code to call feature 1
+            //code to call feature 2
 
         }
     }
